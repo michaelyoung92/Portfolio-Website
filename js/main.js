b878cda10eda95f61fb24f6551f4bc58e0b7f93c
michaelyoung92 <michaelbyoung9208@gmail.com>
2023-03-20 20:08:22 +0000

    animations for hero section complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,44 @@
-gsap.from("header", { y: -100, ease: "back", duration: 1 });
-gsap.from("footer", { y: 100, ease: "back", duration: 1 });
-
-const myText = new SplitType(".primary-heading");
+const primaryHeading = new SplitType(".primary-heading");
 
 gsap.to(".char", {
   y: 0,
   stagger: 0.05,
   delay: 0.2,
   duration: 0.1,
+});
+
+gsap.from("header", {
+  duration: 1.1,
+  delay: 5,
+  opacity: 0,
+});
+
+gsap.from("footer", {
+  duration: 1.1,
+  delay: 5,
+  opacity: 0,
+});
+
+gsap.from(".anim1", {
+  duration: 1,
+  opacity: 0,
+  y: -50,
+  delay: 1.6,
+  stagger: 0.6,
+});
+
+gsap.from(".hero-img", {
+  duration: 1.2,
+  opacity: 0,
+  y: 50,
+  delay: 5,
+  ease: "back",
+});
+
+gsap.to(".button", {
+  duration: 0.5,
+  scale: 1.1,
+  yoyo: true,
+  repeat: 1,
+  delay: 4,
 });
